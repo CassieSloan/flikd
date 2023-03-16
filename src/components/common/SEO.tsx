2b8bc0f9fbcef7d629d4413e504370a5754378c7
Cassie <cassandra.sloan16@gmail.com>
2023-03-16 22:52:45 +1100

    security: add noindex

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -17,6 +17,7 @@ const SEO: FC<SEOProps> = ({ title, description }: SEOProps) => {
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="robots" content="noindex" />
     </Head>
   );
 };
