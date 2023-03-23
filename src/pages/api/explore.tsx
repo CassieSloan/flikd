2266ccdcfaf09c5d82b1e5f5d2b8eb530ebe6a21
Cassie <cassandra.sloan16@gmail.com>
2023-03-23 23:38:03 +1100

    replace empty file

---
+++ src/pages/api/explore.tsx
@@ -0,0 +1,13 @@
+// type ComponentProps = {
+// 	propName: propType;
+// };
+/**
+ * Render Component component.
+ */
+export const Expore = () => {
+	return (
+		<section>
+			<h1>a component</h1>
+		</section>
+	);
+};
