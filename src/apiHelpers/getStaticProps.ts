db68b2ec9c8ad84e0750235cf2591c42276e174e
Cassie <cassandra.sloan16@gmail.com>
2023-03-26 22:03:39 +1100

    remove empty file

--- src/apiHelpers/getStaticProps.ts
+++
@@ -1,8 +0,0 @@
-// /**
-//  * Get Server Side Props.
-//  */
-// export async function getServerSideProps(context: any) {
-// 	return {
-// 		props: {}, // will be passed to the page component as props
-// 	};
-// }
